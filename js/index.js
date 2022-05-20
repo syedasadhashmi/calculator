258ae3eec469ae3d8a2e892642cdc2b4d2863dcb
sshashmi <sshashmi@NIS-PK132s-MacBook-Pro.local>
2022-05-20 19:03:51 +0500

    clean and indentation applied to code

--- js/index.js
+++ js/index.js
@@ -1,24 +1,18 @@
-console.log('connected');
 
 let showOutput = document.getElementById('display-screen');
-console.log(showOutput);
-function functionVal(num){
-    console.log(num);
+function functionVal(num) {
     showOutput.value += num;
 }
 
-function calculateVal(){
-    console.log(showOutput.value);
-    let result= eval(showOutput.value);
-    showOutput.value = result;    
-    
+function calculateVal() {
+    let result = eval(showOutput.value);
+    showOutput.value = result;
 }
-function clearFunc(){
+function clearFunc() {
     showOutput.value = " ";
 }
-function sliceFunc(){
+function sliceFunc() {
     let del = showOutput.value;
-    console.log(del.slice(0,-1));
-    let newVal = del.slice(0,-1);
+    let newVal = del.slice(0, -1);
     showOutput.value = newVal;
 }
